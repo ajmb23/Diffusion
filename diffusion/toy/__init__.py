4b21ba4d735998572321eea346425aa47952936a
My <a.bourdin246@gmail.com>
2023-12-30 11:29:50 -0400

    Commit

--- diffusion/toy/__init__.py
+++ diffusion/toy/__init__.py
@@ -1,2 +1,2 @@
 from diffusion.toy.distributions import gaussians, two_moons
-from diffusion.toy.plotting import forward_sde, forward_sde_dist, reverse_sde, reverse_sde_nn, sampling 
+from diffusion.toy.plotting import forward_sde, forward_sde_dist, reverse_sde, reverse_sde_nn, sample 
